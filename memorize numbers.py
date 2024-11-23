012b71dd22b9bac50540d1b8648dd054078e4293
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2024-11-24 04:32:28 +0530

    changes

--- memorize numbers.py
+++ memorize numbers.py
@@ -3,6 +3,8 @@ import time
 import signal
 from random import Random
 
+import numpy
+
 # Load the JSON file and convert it into a dictionary
 with open('number_mnemonics.json', 'r') as file:
     mnemonics_dict = json.load(file)
@@ -20,26 +22,23 @@ signal.signal(signal.SIGALRM, timeout_handler)
 rights = 0
 numbers_count = int(input("How many numbers would you like to recall? "))
 is_random = input("Would you like the numbers to be random? (y/n) ")
-initial_milliseconds = 5000
-remaining_milliseconds = initial_milliseconds
 
+timing = []
 for i in range(numbers_count):
     if is_random == "y":
         random_num = Random().randint(0, numbers_count - 1)
     else:
         random_num = i
-    print("Time Remaining: " + str(remaining_milliseconds / 1000) + " seconds")
+    print("Time Remaining: 5 seconds")
     print("Enter the mnemonic for the number " + str(random_num).zfill(2) + ": ")
 
     start_time = time.time() * 1000
-    signal.setitimer(signal.ITIMER_REAL, remaining_milliseconds / 1000)
+    signal.setitimer(signal.ITIMER_REAL, 5)
 
     mnemonic = input()
     signal.setitimer(signal.ITIMER_REAL, 0)
     end_time = time.time() * 1000
-
-    elapsed_time = end_time - start_time
-    remaining_milliseconds = max(0, remaining_milliseconds - elapsed_time) + initial_milliseconds
+    timing.append(end_time - start_time)
 
     if mnemonic == mnemonics_dict[str(random_num).zfill(2)]:
         print("Correct!")
@@ -49,3 +48,5 @@ for i in range(numbers_count):
         i -= 1
 
 print("You got " + str(rights) + " out of " + str(numbers_count) + " right.")
+average_time = numpy.average(timing)
+print("Average time taken per number: " + str(average_time/1000) + " seconds.")
