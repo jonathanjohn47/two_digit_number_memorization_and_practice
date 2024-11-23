9262d229f88e84a7cef1425656e3ccb384594968
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2024-11-23 14:50:34 +0530

    changes

--- memorize numbers.py
+++ memorize numbers.py
@@ -1,8 +1,44 @@
 import json
+import time
+import signal
+from random import Random
 
-# Load the JSON file
+# Load the JSON file and convert it into a dictionary
 with open('number_mnemonics.json', 'r') as file:
-    mnemonics = json.load(file)
+    mnemonics_dict = json.load(file)
 
-# Print the content
-print(json.dumps(mnemonics, indent=4))
+
+# Function to handle the timeout
+def timeout_handler(signum, frame):
+    print("\nTime's up! You took too long to respond.")
+    exit(1)
+
+
+# Set the signal handler for SIGALRM
+signal.signal(signal.SIGALRM, timeout_handler)
+
+rights = 0
+
+numbers_count = 3
+
+seconds = 7
+
+for i in range(numbers_count):
+    random_num = Random().randint(0, 99)
+    print("Enter the mnemonic for the number " + str(random_num).zfill(2) + ": ")
+
+    # Set the alarm for 5 seconds
+    signal.alarm(seconds)
+
+    mnemonic = input()
+    # Disable the alarm
+    signal.alarm(0)
+
+    if mnemonic == mnemonics_dict[str(random_num).zfill(2)]:
+        print("Correct!")
+        rights += 1
+    else:
+        print("Wrong mnemonic entered. Please try again.")
+        i -= 1
+
+print("You got " + str(rights) + " out of " + str(numbers_count) + " right.")
